9419048124552f76d1d9ca2b1fc27ef6ff043884
philip <anlanderp@gmail.com>
2019-04-09 18:40:33 +0200

    ,

--- jslabbtwo/src/newmovie.js
+++ jslabbtwo/src/newmovie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
+import axiosCancel from 'axios-cancel';
 
 class Newmovie extends Component {
   constructor(){
@@ -27,45 +28,41 @@ class Newmovie extends Component {
         title:this.state.title,
         director:this.state.director,
         description:this.state.description,
-        rating:this.state.rating
+        rating:this.state.rating,
       }
       axios.post("http://ec2-13-53-132-57.eu-north-1.compute.amazonaws.com:3000/movies/",movie)
       .then(res=>console.log(res))
-    }
 
-
-    OnCancel(e) {
-      e.preventDefault();
       const url = "http://ec2-13-53-132-57.eu-north-1.compute.amazonaws.com:3000"
       this.source = axios.CancelToken.source();
 
-      const movie = {
-        id: this.state.id,
-        title: this.state.title,
-        description: this.state.description,
-        director: this.state.director,
-        rating: this.state.rating
-      }
-
-      axios.put(url + "/movies/", {
+      axios.get(url + "/movies/", {
         cancelToken: this.source.token,
       })
         .then(res => {
           console.log(res)
-          this.setState({ redirect: true })
         })
         .catch(err => {
           console.log(err)
-          alert("Title must be between 1 and 40 characters, description between 1 and 300 characters, Rating between 0-5");
+          if (axios.isCancel(err)){
+            alert("Title must be between 1 and 40 characters, description between 1 and 300 characters, Rating between 0-5");
+          } else {
+            return
+          }
         })
+
     }
+
+
+
+
 
 
 
   render(){
 
     return (
-      <form className="form-new">
+      <form className="form-new" onSubmit={this.Oncancel}>
         <input className="input-title" name="title" type="text" placeholder='Title'  onChange={(e)=> this.getinputvalue(e)}/>
         <input className="input-director" name="director" type="text" placeholder='Director' onChange={(e)=> this.getinputvalue(e)}/>
         <textarea className="input-textarea" name="description" type="text" placeholder='Description' onChange={(e)=> this.getinputvalue(e)}></textarea>
